79e6a0794b0fcde321c242cc0219428dda3184c7
s0bczynski <sobczynski.mateusz@o2.pl>
2023-11-03 20:02:45 +0100

    made IContactService interface

--- ContactAPI/Services/ContactService/IContactService.cs
+++ ContactAPI/Services/ContactService/IContactService.cs
@@ -2,6 +2,11 @@
 {
 	public interface IContactService
 	{
+		List<Contact> GetAllContacts();
+		Contact GetSingleContact(int id);
+		List<Contact> AddContact(Contact contact);
+		List<Contact> UpdateContact(int id, Contact request);
+		List<Contact> DeleteContact(int id);
 
 	}
 }
